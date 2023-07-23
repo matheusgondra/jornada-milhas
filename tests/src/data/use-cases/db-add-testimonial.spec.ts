70549077eedea73dc96bfbdfc6bbb89d082147a9
Matheus de Gondra <matheusgondra951@gmail.com>
2023-07-23 13:34:56 -0300

    refactor: move sut creation to factory method

--- tests/src/data/use-cases/db-add-testimonial.spec.ts
+++ tests/src/data/use-cases/db-add-testimonial.spec.ts
@@ -2,27 +2,45 @@ import { AddTestimonialRepository } from "../../../../src/data/protocols";
 import { DbAddTestimonial } from "../../../../src/data/use-cases";
 import { AddTestimonialModel, TestimonialModel } from "../../../../src/domain";
 
+const makeAddTestimonialRepositoryStub = (): AddTestimonialRepository => {
+	class AddTestimonialRepositoryStub implements AddTestimonialRepository {
+		async add(data: AddTestimonialModel): Promise<TestimonialModel> {
+			return {
+				id: 1,
+				name: "any_name",
+				photo: "any_photo",
+				testimonial: "any_testimonial"
+			};
+		}
+	}
+	return new AddTestimonialRepositoryStub();
+};
+
+interface SutTypes {
+	sut: DbAddTestimonial;
+	addTestimonialRepositoryStub: AddTestimonialRepository;
+}
+
+const makeSut = (): SutTypes => {
+	const addTestimonialRepositoryStub = makeAddTestimonialRepositoryStub();
+	const sut = new DbAddTestimonial(addTestimonialRepositoryStub);
+	return {
+		sut,
+		addTestimonialRepositoryStub
+	};
+};
+
+const makeFakeData = (): AddTestimonialModel => ({
+	name: "any_name",
+	photo: "any_photo",
+	testimonial: "any_testimonial"
+});
+
 describe("DbAddTestimonial", () => {
 	it("Should call AddTestimonialRepository with correct values", async () => {
-		class AddTestimonialRepositoryStub implements AddTestimonialRepository {
-			async add(data: AddTestimonialModel): Promise<TestimonialModel> {
-				return {
-					id: 1,
-					name: "any_name",
-					photo: "any_photo",
-					testimonial: "any_testimonial"
-				};
-			}
-		}
-		const addTestimonialRepositoryStub = new AddTestimonialRepositoryStub();
-		const sut = new DbAddTestimonial(addTestimonialRepositoryStub);
+		const { sut, addTestimonialRepositoryStub } = makeSut();
 		const addSpy = jest.spyOn(addTestimonialRepositoryStub, "add");
-		const data = {
-			name: "any_name",
-			photo: "any_photo",
-			testimonial: "any_testimonial"
-		};
-		await sut.add(data);
-		expect(addSpy).toBeCalledWith(data);
+		await sut.add(makeFakeData());
+		expect(addSpy).toBeCalledWith(makeFakeData());
 	});
 });
