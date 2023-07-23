2452255b4205b3e6f8083bbd4fb34501e9d33aa1
Matheus de Gondra <matheusgondra951@gmail.com>
2023-07-23 12:39:49 -0300

    refactor: remove code duplication

--- tests/src/presentation/controllers/add-testimonial-controller.spec.ts
+++ tests/src/presentation/controllers/add-testimonial-controller.spec.ts
@@ -2,6 +2,7 @@ import { AddTestimonialController } from "../../../../src/presentation/controlle
 import { MissingParamError } from "../../../../src/presentation/errors";
 import { badRequest } from "../../../../src/presentation/helpers/http-helpers";
 import { Validation } from "../../../../src/presentation/helpers/validators/validation";
+import { HttpRequest } from "../../../../src/presentation/protocols/http";
 
 const makeValidationStub = (): Validation => {
 	class ValidationStub implements Validation {
@@ -10,6 +11,16 @@ const makeValidationStub = (): Validation => {
 		}
 	}
 	return new ValidationStub();
+};
+
+const makeFakeRequest = (): HttpRequest => {
+	return {
+		body: {
+			name: "any_name",
+			photo: "any_photo",
+			testimonial: "any_testimonial"
+		}
+	};
 };
 
 interface SutTypes {
@@ -30,15 +41,8 @@ describe("AddTestimonial Controller", () => {
 	it("Should call Validation with correct values", async () => {
 		const { sut, validationStub } = makeSut();
 		const validateSpy = jest.spyOn(validationStub, "validate");
-		const httpRequest = {
-			body: {
-				name: "any_name",
-				photo: "any_photo",
-				testimonial: "any_testimonial"
-			}
-		};
-		await sut.handle(httpRequest);
-		expect(validateSpy).toBeCalledWith(httpRequest.body);
+		await sut.handle(makeFakeRequest());
+		expect(validateSpy).toBeCalledWith(makeFakeRequest().body);
 	});
 
 	it("Should return 400 if validation return error", async () => {
@@ -46,14 +50,7 @@ describe("AddTestimonial Controller", () => {
 		jest
 			.spyOn(validationStub, "validate")
 			.mockReturnValueOnce(new MissingParamError("any_field"));
-		const httpResquest = {
-			body: {
-				name: "any_name",
-				photo: "any_photo",
-				testimonial: "any_testimonial"
-			}
-		};
-		const httpResponse = await sut.handle(httpResquest);
+		const httpResponse = await sut.handle(makeFakeRequest());
 		expect(httpResponse).toEqual(
 			badRequest(new MissingParamError("any_field"))
 		);
