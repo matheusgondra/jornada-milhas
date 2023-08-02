8498f001af72e95b0c412ce913a6f6d75161aeb5
Matheus de Gondra <matheusgondra951@gmail.com>
2023-08-01 22:31:40 -0300

    test: ensure requests return xml content type when forced

--- tests/src/main/middlewares/content-type.test.ts
+++ tests/src/main/middlewares/content-type.test.ts
@@ -10,4 +10,14 @@ describe("Content Type Middleware", () => {
 			.get("/test-content-type")
 			.expect("content-type", /json/);
 	});
+
+	it("Should return xml content type when forced", async () => {
+		app.get("/test-content-type-xml", (req, res) => {
+			res.type("xml");
+			res.send("");
+		});
+		await request(app)
+			.get("/test-content-type-xml")
+			.expect("content-type", /xml/);
+	});
 });
