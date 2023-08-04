72655b727ce5cb980a7376003fab06fa19fa306d
Matheus de Gondra <matheusgondra951@gmail.com>
2023-08-03 23:14:04 -0300

    refactor: move sut creation to a factory method

--- tests/src/presentation/controllers/load-testimonial.spec.ts
+++ tests/src/presentation/controllers/load-testimonial.spec.ts
@@ -1,20 +1,39 @@
 import { LoadTestimonialController } from "../../../../src/presentation/controllers/load-testimonial";
 import { Validation } from "../../../../src/presentation/helpers/validators/validation";
+import { HttpRequest } from "../../../../src/presentation/protocols/http";
+
+const makeValidationStub = (): Validation => {
+	class ValidationStub implements Validation {
+		validate(input: any): Error | null {
+			return null;
+		}
+	}
+	return new ValidationStub();
+};
+
+interface SutTypes {
+	sut: LoadTestimonialController;
+	validationStub: Validation;
+}
+
+const makeSut = (): SutTypes => {
+	const validationStub = makeValidationStub();
+	const sut = new LoadTestimonialController({ validation: validationStub });
+	return {
+		sut,
+		validationStub
+	};
+};
+
+const makeFakeRequest = (): HttpRequest => ({
+	body: ""
+});
 
 describe("LoadTestimonialController", () => {
 	it("Should call Validation with correct values", async () => {
-		class ValidationStub implements Validation {
-			validate(input: any): Error | null {
-				return null;
-			}
-		}
-		const validationStub = new ValidationStub();
-		const sut = new LoadTestimonialController({ validation: validationStub });
+		const { sut, validationStub } = makeSut();
 		const validateSpy = jest.spyOn(validationStub, "validate");
-		const httpRequest = {
-			body: ""
-		};
-		await sut.handle(httpRequest);
-		expect(validateSpy).toBeCalledWith(httpRequest);
+		await sut.handle(makeFakeRequest());
+		expect(validateSpy).toBeCalledWith(makeFakeRequest());
 	});
 });
