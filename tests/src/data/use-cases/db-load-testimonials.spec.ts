ba5f5f9df879264ca9acb250e62a96f3856c4aca
Matheus de Gondra <matheusgondra951@gmail.com>
2023-08-05 08:16:57 -0300

    refactor: move sut creation to a factory method

--- tests/src/data/use-cases/db-load-testimonials.spec.ts
+++ tests/src/data/use-cases/db-load-testimonials.spec.ts
@@ -1,28 +1,41 @@
 import { LoadTestimonialsRepository } from "../../../../src/data/protocols/load-testimonials-repository";
 import { DbLoadTestimonials } from "../../../../src/data/use-cases/db-load-testimonials";
 
+const makeLoadTestimonialsRepositoryStub = (): LoadTestimonialsRepository => {
+	class LoadTestimonialsRepositoryStub implements LoadTestimonialsRepository {
+		async load(): Promise<LoadTestimonialsRepository.Result> {
+			return [
+				{
+					id: 1,
+					name: "any_name",
+					photo: "any_photo",
+					testimonial: "any_testimonial"
+				}
+			];
+		}
+	}
+	return new LoadTestimonialsRepositoryStub();
+};
+
+interface SutTypes {
+	sut: DbLoadTestimonials;
+	loadTestimonialsRepositoryStub: LoadTestimonialsRepository;
+}
+
+const makeSut = (): SutTypes => {
+	const loadTestimonialsRepositoryStub = makeLoadTestimonialsRepositoryStub();
+	const sut = new DbLoadTestimonials({
+		loadTestimonialsRepository: loadTestimonialsRepositoryStub
+	});
+	return {
+		sut,
+		loadTestimonialsRepositoryStub
+	};
+};
+
 describe("DbLoadTestimonials", () => {
 	it("Should call LoadTestimonialsRepository", async () => {
-		class LoadTestimonialsRepositoryStub
-			implements LoadTestimonialsRepository
-		{
-			async load(): Promise<LoadTestimonialsRepository.Result> {
-				return [
-					{
-						id: 1,
-						name: "any_name",
-						photo: "any_photo",
-						testimonial: "any_testimonial"
-					}
-				];
-			}
-		}
-		const loadTestimonialsRepositoryStub =
-			new LoadTestimonialsRepositoryStub();
-		new LoadTestimonialsRepositoryStub();
-		const sut = new DbLoadTestimonials({
-			loadTestimonialsRepository: loadTestimonialsRepositoryStub
-		});
+		const { sut, loadTestimonialsRepositoryStub } = makeSut();
 		const loadSpy = jest.spyOn(loadTestimonialsRepositoryStub, "load");
 		await sut.load();
 		expect(loadSpy).toBeCalledTimes(1);
