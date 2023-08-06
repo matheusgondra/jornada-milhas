318e3355292c26086dd4356797c17bd907134439
Matheus de Gondra <matheusgondra951@gmail.com>
2023-08-06 12:20:59 -0300

    feat: ensure TestimonialRepository return the testimonials on succcess

--- src/infra/db/postgresql/testimonial-repository.ts
+++ src/infra/db/postgresql/testimonial-repository.ts
@@ -1,7 +1,10 @@
 import { AddTestimonialRepository } from "../../../data/protocols";
+import { LoadTestimonials } from "../../../domain";
 import { db } from "./prisma";
 
-export class TestimonialRepository implements AddTestimonialRepository {
+export class TestimonialRepository
+	implements AddTestimonialRepository, LoadTestimonials
+{
 	async add(
 		data: AddTestimonialRepository.Params
 	): Promise<AddTestimonialRepository.Result> {
@@ -10,5 +13,10 @@ export class TestimonialRepository implements AddTestimonialRepository {
 		});
 
 		return testimonial;
+	}
+
+	async load(): Promise<LoadTestimonials.Result> {
+		const testimonials = await db.testimonial.findMany();
+		return testimonials;
 	}
 }
