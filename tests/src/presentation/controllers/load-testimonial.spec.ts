a5ce18715d6860785be741737b41c2ee7b554b3e
Matheus de Gondra <matheusgondra951@gmail.com>
2023-08-11 08:52:02 -0300

    refactor: move sut creation to helper factory method

--- tests/src/presentation/controllers/load-testimonial.spec.ts
+++ tests/src/presentation/controllers/load-testimonial.spec.ts
@@ -1,20 +1,36 @@
 import { LoadTestimonialController } from "../../../../src/presentation/controllers/load-testimonial";
 import { Validation } from "../../../../src/presentation/helpers/validators/validation";
 
+const makeValidationStub = (): Validation => {
+	class ValidationStub implements Validation {
+		validate(input: any): Error | null {
+			return null;
+		}
+	}
+	return new ValidationStub();
+};
+
+interface SutTypes {
+	sut: LoadTestimonialController;
+	validationStub: Validation;
+}
+
+const makeSut = (): SutTypes => {
+	const validationStub = makeValidationStub();
+	const sut = new LoadTestimonialController({
+		validation: validationStub
+	});
+	return {
+		sut,
+		validationStub
+	};
+};
+
 describe("LoadTestimonial Controller", () => {
 	it("Should call Validation with correct values", async () => {
-		class ValidationStub implements Validation {
-			validate(input: any): Error | null {
-				return null;
-			}
-		}
-		const validationStub = new ValidationStub();
-		const sut = new LoadTestimonialController({
-			validation: validationStub
-		});
+		const { sut, validationStub } = makeSut();
 		const validateSpy = jest.spyOn(validationStub, "validate");
 		const httpRequest = {
-			body: {},
 			params: {
 				testimonialId: 1
 			}
