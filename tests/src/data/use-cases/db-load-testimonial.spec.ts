4f924f9a776c4fd4918e1254e871243bd8d9e78a
Matheus de Gondra <matheusgondra951@gmail.com>
2023-08-14 12:58:34 -0300

    refactor: move sut creation to helper factory method

--- tests/src/data/use-cases/db-load-testimonial.spec.ts
+++ tests/src/data/use-cases/db-load-testimonial.spec.ts
@@ -1,24 +1,41 @@
 import { DbLoadTestimonial } from "../../../../src/data/use-cases";
 import { LoadTestimonial } from "../../../../src/domain";
 
+const makeLoadTestimonialRepositoryStub = (): LoadTestimonial => {
+	class LoadTestimonialRepositoryStub implements LoadTestimonial {
+		async load(
+			testimonialId: LoadTestimonial.Params
+		): Promise<LoadTestimonial.Result> {
+			return {
+				id: 1,
+				name: "any_name",
+				photo: "any_photo",
+				testimonial: "any_testimonial"
+			};
+		}
+	}
+	return new LoadTestimonialRepositoryStub();
+};
+
+interface SutTypes {
+	sut: DbLoadTestimonial;
+	loadTestimonialRepositoryStub: LoadTestimonial;
+}
+
+const makeSut = (): SutTypes => {
+	const loadTestimonialRepositoryStub = makeLoadTestimonialRepositoryStub();
+	const sut = new DbLoadTestimonial({
+		loadTestimonialRepository: loadTestimonialRepositoryStub
+	});
+	return {
+		sut,
+		loadTestimonialRepositoryStub
+	};
+};
+
 describe("DbLoadTestimonial", () => {
 	it("Should call LoadTestimonialRepository", async () => {
-		class LoadTestimonialRepositoryStub implements LoadTestimonial {
-			async load(
-				testimonialId: LoadTestimonial.Params
-			): Promise<LoadTestimonial.Result> {
-				return {
-					id: 1,
-					name: "any_name",
-					photo: "any_photo",
-					testimonial: "any_testimonial"
-				};
-			}
-		}
-		const loadTestimonialRepositoryStub = new LoadTestimonialRepositoryStub();
-		const sut = new DbLoadTestimonial({
-			loadTestimonialRepository: loadTestimonialRepositoryStub
-		});
+		const { sut, loadTestimonialRepositoryStub } = makeSut();
 		const loadSpy = jest.spyOn(loadTestimonialRepositoryStub, "load");
 		await sut.load(1);
 		expect(loadSpy).toBeCalledWith(1);
