91cd4e8224a238ec897e7cea8717bddf0f727b57
Matheus de Gondra <matheusgondra951@gmail.com>
2023-08-15 13:02:46 -0300

    refactor: moving testimonial routes to single file

--- src/main/routes/add-testimonial-routes.ts
+++
@@ -1,7 +0,0 @@
-import { Router } from "express";
-import { makeAddTestimonial } from "../factories";
-import { adaptRoute } from "../adapters/express-router-adapter";
-
-export default (router: Router): void => {
-	router.post("/depoimentos", adaptRoute(makeAddTestimonial()));
-};
--- src/main/routes/testimonial-routes.ts
+++ src/main/routes/testimonial-routes.ts
@@ -1,7 +1,9 @@
 import { Router } from "express";
 import { adaptRoute } from "../adapters/express-router-adapter";
 import { makeLoadTestimonials } from "../factories/load-testimonials";
+import { makeAddTestimonial } from "../factories";
 
 export default (router: Router): void => {
+	router.post("/depoimentos", adaptRoute(makeAddTestimonial()));
 	router.get("/depoimentos", adaptRoute(makeLoadTestimonials()));
 };
