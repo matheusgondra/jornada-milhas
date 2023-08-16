7f2fc97c0777e5973a2bb96c512591cf0444fad3
Matheus de Gondra <matheusgondra951@gmail.com>
2023-08-16 13:01:57 -0300

    feat: ensure UpdateTestimonialController return 400 if Validation returns an error

--- src/presentation/controllers/update-testimonial.ts
+++ src/presentation/controllers/update-testimonial.ts
@@ -1,4 +1,4 @@
-import { Validation, success } from "../helpers";
+import { Validation, badRequest, success } from "../helpers";
 import { Controller, HttpRequest, HttpResponse } from "../protocols";
 
 export class UpdateTestimonialController implements Controller {
@@ -9,7 +9,10 @@ export class UpdateTestimonialController implements Controller {
 	}
 
 	async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-		this.validation.validate(httpRequest);
+		const error = this.validation.validate(httpRequest);
+		if (error) {
+			return badRequest(error);
+		}
 
 		return success("");
 	}
