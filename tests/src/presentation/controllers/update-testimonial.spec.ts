39bacc4d4869c26bf090be367725e7589b85df58
Matheus de Gondra <matheusgondra951@gmail.com>
2023-08-16 12:58:42 -0300

    refactor: move sut creation to helper factory method

--- tests/src/presentation/controllers/update-testimonial.spec.ts
+++ tests/src/presentation/controllers/update-testimonial.spec.ts
@@ -1,17 +1,34 @@
 import { UpdateTestimonialController } from "../../../../src/presentation/controllers";
 import { Validation } from "../../../../src/presentation/helpers";
 
+const makeValidationStub = (): Validation => {
+	class ValidationStub implements Validation {
+		validate(input: any): Error | null {
+			return null;
+		}
+	}
+	return new ValidationStub();
+};
+
+interface SutTypes {
+	sut: UpdateTestimonialController;
+	validationStub: Validation;
+}
+
+const makeSut = (): SutTypes => {
+	const validationStub = makeValidationStub();
+	const sut = new UpdateTestimonialController({
+		validation: validationStub
+	});
+	return {
+		sut,
+		validationStub
+	};
+};
+
 describe("UpdateTestimonial", () => {
 	it("Should call Validation with correct values", async () => {
-		class ValidationStub implements Validation {
-			validate(input: any): Error | null {
-				return null;
-			}
-		}
-		const validationStub = new ValidationStub();
-		const sut = new UpdateTestimonialController({
-			validation: validationStub
-		});
+		const { sut, validationStub } = makeSut();
 		const validateSpy = jest.spyOn(validationStub, "validate");
 		const httpRequest = {
 			body: {
