84090599eaedd67d468beba3756e56041e234646
Matheus de Gondra <matheusgondra951@gmail.com>
2023-09-11 13:31:08 -0300

    test: ensure testimonial route PATCH return 200 on success

--- tests/src/main/routes/testimonial-routes.test.ts
+++ tests/src/main/routes/testimonial-routes.test.ts
@@ -2,6 +2,12 @@ import request from "supertest";
 import app from "../../../../src/main/config/app";
 import { PrismaHelper } from "../../../helpers/prisma-helper";
 
+const makeFakeRequest = () => ({
+	name: "John",
+	photo: "imagem.png",
+	testimonial: "Achei muito patetico"
+});
+
 describe("Testimonial Routes", () => {
 	beforeEach(async () => {
 		await PrismaHelper.connect();
@@ -15,31 +21,21 @@ describe("Testimonial Routes", () => {
 		it("Should returns an testimonial on success", async () => {
 			await request(app)
 				.post("/depoimentos")
-				.send({
-					name: "John",
-					photo: "https://lh3.googleusercontent.com/pw/AIL4fc9UlFXaK0oQ9WxcqfNibYXxSEsnFI0zrH5EYk3JjHqbsIB3GCJ--qNBm5GRpYfDml2W2s55AgrvQh82U71_jlLuhesbGT7n_L7gFNtv2goPU3-udEgHYSYEuKaZbWlOSnj-6klCT-fMRyxcgExFQdwdK3eHtQnimqWYIsJLJ_hVrLBZnWMwslpIIe1wsKTkSIu-3_qNS7GA3IOzha6fuqOYttuBsbObjKSznyWcgOGBR992DbhhDRidVCCy44NcdIA0PtC2emyBm7ec-6938a_lscGhMz1g2YVr-wvkRua7SwW6b-usDh8hsARb6aqxUoWanSwriDEqegoCVCS6unfQLp3A0v2GLvjFkiXp-tzfJKlV9S-1-Q5tju6MadWORxX-BpT4DE7jf7LaqOp86uOtaQ-MStUWSTNtdK1f8wcOohzmAudutEdcj_Y9unuVkTsujq6ldsQtqNaSL9PUMDiENS5QvAVnseYOgs7rBkoBwveT-E_p8qjWYi-FRqCtHqtMRp0aCuyGTfjF0L68kKESyfANIseYyHvYkNoJ4CrH4PGIEI7lr3fhKafJdyq08AiaCHTvfZDlbnsRKFXM4WUcN6RFEvs8-8sDOcaDUzR3YTQev3kFH6mQEuKmNiHX_e_DrweiJrQ2snvn9ao2GFtD7oWuJtq8-x1bbxPwDuM2FW5wuAz2MriW_1p2sZDdB-BCnAThpA91sNizo8P7bjvTEOluMv0FMv7xJYDwolGhzuaGzjTv7gCsyyYBRCea55MmlEzzCl65NCuglqsiTj9JGHq7qqUD53kmG3G_K2SSIVyYWxPBLLZq41sVdpXatN2VzAnkx5Forr-AGpbRk9c1V4lK8JbsyTsM-kuOHVhHZETWLT6-dHI8Yt-bfRBI1q8RsmMa_UhBgzuFmPZc1u9SCbc=w1048-h923-s-no?authuser=0",
-					testimonial: "Achei muito patetico"
-				})
+				.send(makeFakeRequest())
 				.expect(201);
 		});
 	});
 
 	describe("GET /depoimentos", () => {
 		it("Should returns the testimonials on success", async () => {
-			await request(app).post("/depoimentos").send({
-				name: "John",
-				photo: "https://lh3.googleusercontent.com/pw/AIL4fc9UlFXaK0oQ9WxcqfNibYXxSEsnFI0zrH5EYk3JjHqbsIB3GCJ--qNBm5GRpYfDml2W2s55AgrvQh82U71_jlLuhesbGT7n_L7gFNtv2goPU3-udEgHYSYEuKaZbWlOSnj-6klCT-fMRyxcgExFQdwdK3eHtQnimqWYIsJLJ_hVrLBZnWMwslpIIe1wsKTkSIu-3_qNS7GA3IOzha6fuqOYttuBsbObjKSznyWcgOGBR992DbhhDRidVCCy44NcdIA0PtC2emyBm7ec-6938a_lscGhMz1g2YVr-wvkRua7SwW6b-usDh8hsARb6aqxUoWanSwriDEqegoCVCS6unfQLp3A0v2GLvjFkiXp-tzfJKlV9S-1-Q5tju6MadWORxX-BpT4DE7jf7LaqOp86uOtaQ-MStUWSTNtdK1f8wcOohzmAudutEdcj_Y9unuVkTsujq6ldsQtqNaSL9PUMDiENS5QvAVnseYOgs7rBkoBwveT-E_p8qjWYi-FRqCtHqtMRp0aCuyGTfjF0L68kKESyfANIseYyHvYkNoJ4CrH4PGIEI7lr3fhKafJdyq08AiaCHTvfZDlbnsRKFXM4WUcN6RFEvs8-8sDOcaDUzR3YTQev3kFH6mQEuKmNiHX_e_DrweiJrQ2snvn9ao2GFtD7oWuJtq8-x1bbxPwDuM2FW5wuAz2MriW_1p2sZDdB-BCnAThpA91sNizo8P7bjvTEOluMv0FMv7xJYDwolGhzuaGzjTv7gCsyyYBRCea55MmlEzzCl65NCuglqsiTj9JGHq7qqUD53kmG3G_K2SSIVyYWxPBLLZq41sVdpXatN2VzAnkx5Forr-AGpbRk9c1V4lK8JbsyTsM-kuOHVhHZETWLT6-dHI8Yt-bfRBI1q8RsmMa_UhBgzuFmPZc1u9SCbc=w1048-h923-s-no?authuser=0",
-				testimonial: "Achei muito patetico"
-			});
+			await request(app).post("/depoimentos").send(makeFakeRequest());
 
 			const response = await request(app).get("/depoimentos");
 			expect(response.statusCode).toBe(200);
 			expect(response.body).toEqual([
 				{
 					id: 1,
-					name: "John",
-					photo: "https://lh3.googleusercontent.com/pw/AIL4fc9UlFXaK0oQ9WxcqfNibYXxSEsnFI0zrH5EYk3JjHqbsIB3GCJ--qNBm5GRpYfDml2W2s55AgrvQh82U71_jlLuhesbGT7n_L7gFNtv2goPU3-udEgHYSYEuKaZbWlOSnj-6klCT-fMRyxcgExFQdwdK3eHtQnimqWYIsJLJ_hVrLBZnWMwslpIIe1wsKTkSIu-3_qNS7GA3IOzha6fuqOYttuBsbObjKSznyWcgOGBR992DbhhDRidVCCy44NcdIA0PtC2emyBm7ec-6938a_lscGhMz1g2YVr-wvkRua7SwW6b-usDh8hsARb6aqxUoWanSwriDEqegoCVCS6unfQLp3A0v2GLvjFkiXp-tzfJKlV9S-1-Q5tju6MadWORxX-BpT4DE7jf7LaqOp86uOtaQ-MStUWSTNtdK1f8wcOohzmAudutEdcj_Y9unuVkTsujq6ldsQtqNaSL9PUMDiENS5QvAVnseYOgs7rBkoBwveT-E_p8qjWYi-FRqCtHqtMRp0aCuyGTfjF0L68kKESyfANIseYyHvYkNoJ4CrH4PGIEI7lr3fhKafJdyq08AiaCHTvfZDlbnsRKFXM4WUcN6RFEvs8-8sDOcaDUzR3YTQev3kFH6mQEuKmNiHX_e_DrweiJrQ2snvn9ao2GFtD7oWuJtq8-x1bbxPwDuM2FW5wuAz2MriW_1p2sZDdB-BCnAThpA91sNizo8P7bjvTEOluMv0FMv7xJYDwolGhzuaGzjTv7gCsyyYBRCea55MmlEzzCl65NCuglqsiTj9JGHq7qqUD53kmG3G_K2SSIVyYWxPBLLZq41sVdpXatN2VzAnkx5Forr-AGpbRk9c1V4lK8JbsyTsM-kuOHVhHZETWLT6-dHI8Yt-bfRBI1q8RsmMa_UhBgzuFmPZc1u9SCbc=w1048-h923-s-no?authuser=0",
-					testimonial: "Achei muito patetico"
+					...makeFakeRequest()
 				}
 			]);
 		});
@@ -47,19 +43,30 @@ describe("Testimonial Routes", () => {
 
 	describe("GET /depoimentos/:testimonialId", () => {
 		it("Should returns the testimonial on success", async () => {
-			await request(app).post("/depoimentos").send({
-				name: "John",
-				photo: "https://lh3.googleusercontent.com/pw/AIL4fc9UlFXaK0oQ9WxcqfNibYXxSEsnFI0zrH5EYk3JjHqbsIB3GCJ--qNBm5GRpYfDml2W2s55AgrvQh82U71_jlLuhesbGT7n_L7gFNtv2goPU3-udEgHYSYEuKaZbWlOSnj-6klCT-fMRyxcgExFQdwdK3eHtQnimqWYIsJLJ_hVrLBZnWMwslpIIe1wsKTkSIu-3_qNS7GA3IOzha6fuqOYttuBsbObjKSznyWcgOGBR992DbhhDRidVCCy44NcdIA0PtC2emyBm7ec-6938a_lscGhMz1g2YVr-wvkRua7SwW6b-usDh8hsARb6aqxUoWanSwriDEqegoCVCS6unfQLp3A0v2GLvjFkiXp-tzfJKlV9S-1-Q5tju6MadWORxX-BpT4DE7jf7LaqOp86uOtaQ-MStUWSTNtdK1f8wcOohzmAudutEdcj_Y9unuVkTsujq6ldsQtqNaSL9PUMDiENS5QvAVnseYOgs7rBkoBwveT-E_p8qjWYi-FRqCtHqtMRp0aCuyGTfjF0L68kKESyfANIseYyHvYkNoJ4CrH4PGIEI7lr3fhKafJdyq08AiaCHTvfZDlbnsRKFXM4WUcN6RFEvs8-8sDOcaDUzR3YTQev3kFH6mQEuKmNiHX_e_DrweiJrQ2snvn9ao2GFtD7oWuJtq8-x1bbxPwDuM2FW5wuAz2MriW_1p2sZDdB-BCnAThpA91sNizo8P7bjvTEOluMv0FMv7xJYDwolGhzuaGzjTv7gCsyyYBRCea55MmlEzzCl65NCuglqsiTj9JGHq7qqUD53kmG3G_K2SSIVyYWxPBLLZq41sVdpXatN2VzAnkx5Forr-AGpbRk9c1V4lK8JbsyTsM-kuOHVhHZETWLT6-dHI8Yt-bfRBI1q8RsmMa_UhBgzuFmPZc1u9SCbc=w1048-h923-s-no?authuser=0",
-				testimonial: "Achei muito patetico"
-			});
+			await request(app).post("/depoimentos").send(makeFakeRequest());
 
 			const response = await request(app).get("/depoimentos/1");
 			expect(response.statusCode).toBe(200);
 			expect(response.body).toEqual({
 				id: 1,
+				...makeFakeRequest()
+			});
+		});
+	});
+
+	describe("PATCH /depoimentos/:testimonialId", () => {
+		it("Should returns 200 on success", async () => {
+			await request(app).post("/depoimentos").send(makeFakeRequest());
+
+			const response = await request(app).patch("/depoimentos/1").send({
+				testimonial: "Achei Bom"
+			});
+			expect(response.statusCode).toBe(200);
+			expect(response.body).toEqual({
+				id: 1,
 				name: "John",
-				photo: "https://lh3.googleusercontent.com/pw/AIL4fc9UlFXaK0oQ9WxcqfNibYXxSEsnFI0zrH5EYk3JjHqbsIB3GCJ--qNBm5GRpYfDml2W2s55AgrvQh82U71_jlLuhesbGT7n_L7gFNtv2goPU3-udEgHYSYEuKaZbWlOSnj-6klCT-fMRyxcgExFQdwdK3eHtQnimqWYIsJLJ_hVrLBZnWMwslpIIe1wsKTkSIu-3_qNS7GA3IOzha6fuqOYttuBsbObjKSznyWcgOGBR992DbhhDRidVCCy44NcdIA0PtC2emyBm7ec-6938a_lscGhMz1g2YVr-wvkRua7SwW6b-usDh8hsARb6aqxUoWanSwriDEqegoCVCS6unfQLp3A0v2GLvjFkiXp-tzfJKlV9S-1-Q5tju6MadWORxX-BpT4DE7jf7LaqOp86uOtaQ-MStUWSTNtdK1f8wcOohzmAudutEdcj_Y9unuVkTsujq6ldsQtqNaSL9PUMDiENS5QvAVnseYOgs7rBkoBwveT-E_p8qjWYi-FRqCtHqtMRp0aCuyGTfjF0L68kKESyfANIseYyHvYkNoJ4CrH4PGIEI7lr3fhKafJdyq08AiaCHTvfZDlbnsRKFXM4WUcN6RFEvs8-8sDOcaDUzR3YTQev3kFH6mQEuKmNiHX_e_DrweiJrQ2snvn9ao2GFtD7oWuJtq8-x1bbxPwDuM2FW5wuAz2MriW_1p2sZDdB-BCnAThpA91sNizo8P7bjvTEOluMv0FMv7xJYDwolGhzuaGzjTv7gCsyyYBRCea55MmlEzzCl65NCuglqsiTj9JGHq7qqUD53kmG3G_K2SSIVyYWxPBLLZq41sVdpXatN2VzAnkx5Forr-AGpbRk9c1V4lK8JbsyTsM-kuOHVhHZETWLT6-dHI8Yt-bfRBI1q8RsmMa_UhBgzuFmPZc1u9SCbc=w1048-h923-s-no?authuser=0",
-				testimonial: "Achei muito patetico"
+				photo: "imagem.png",
+				testimonial: "Achei Bom"
 			});
 		});
 	});
