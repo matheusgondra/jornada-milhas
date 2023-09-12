0371fe0f610df542801d3810b942158e253b7d67
Matheus de Gondra <matheusgondra951@gmail.com>
2023-09-12 13:25:57 -0300

    refactor: move sut creation to a factory method

--- tests/src/presentation/controllers/load-random-testimonials.spec.ts
+++ tests/src/presentation/controllers/load-random-testimonials.spec.ts
@@ -1,36 +1,53 @@
 import { LoadRandomTestimonials } from "../../../../src/domain";
 import { LoadRandomTestimonialsController } from "../../../../src/presentation/controllers";
 
+const makeLoadRandomTestimonials = (): LoadRandomTestimonials => {
+	class LoadRandomTestimonialsStub implements LoadRandomTestimonials {
+		async load(): Promise<LoadRandomTestimonials.Result> {
+			return [
+				{
+					id: 1,
+					name: "any_name",
+					photo: "any_photo",
+					testimonial: "any_testimonial"
+				},
+				{
+					id: 1,
+					name: "any_name",
+					photo: "any_photo",
+					testimonial: "any_testimonial"
+				},
+				{
+					id: 1,
+					name: "any_name",
+					photo: "any_photo",
+					testimonial: "any_testimonial"
+				}
+			];
+		}
+	}
+	return new LoadRandomTestimonialsStub();
+};
+
+interface SutTypes {
+	sut: LoadRandomTestimonialsController;
+	loadRandomTestimonialsStub: LoadRandomTestimonials;
+}
+
+const makeSut = (): SutTypes => {
+	const loadRandomTestimonialsStub = makeLoadRandomTestimonials();
+	const sut = new LoadRandomTestimonialsController({
+		loadRandomTestimonials: loadRandomTestimonialsStub
+	});
+	return {
+		sut,
+		loadRandomTestimonialsStub
+	};
+};
+
 describe("LoadRandomTestimonialsController", () => {
 	it("Should call LoadRandomTestimonials", async () => {
-		class LoadRandomTestimonialsStub implements LoadRandomTestimonials {
-			async load(): Promise<LoadRandomTestimonials.Result> {
-				return [
-					{
-						id: 1,
-						name: "any_name",
-						photo: "any_photo",
-						testimonial: "any_testimonial"
-					},
-					{
-						id: 1,
-						name: "any_name",
-						photo: "any_photo",
-						testimonial: "any_testimonial"
-					},
-					{
-						id: 1,
-						name: "any_name",
-						photo: "any_photo",
-						testimonial: "any_testimonial"
-					}
-				];
-			}
-		}
-		const loadRandomTestimonialsStub = new LoadRandomTestimonialsStub();
-		const sut = new LoadRandomTestimonialsController({
-			loadRandomTestimonials: loadRandomTestimonialsStub
-		});
+		const { sut, loadRandomTestimonialsStub } = makeSut();
 		const loadSpy = jest.spyOn(loadRandomTestimonialsStub, "load");
 		await sut.handle({});
 		expect(loadSpy).toBeCalledTimes(1);
